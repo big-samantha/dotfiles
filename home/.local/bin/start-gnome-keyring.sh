4ba67b325c2c6298f2084c121b666e63c0ca09f8
Zee Alexander <zee@zee.space>
2016-06-08 18:35:59 -0400

    Remove useless script

--- home/.local/bin/start-gnome-keyring.sh
+++
@@ -1,9 +0,0 @@
-#eval $(/usr/bin/gnome-keyring-daemon --start --components=gpg,pkcs11,secrets,ssh)
-#export GNOME_KEYRING_CONTROL GNOME_KEYRING_PID GPG_AGENT_INFO SSH_AUTH_SOCK
-# /usr/bin/gnome-keyring-daemon --start --components=ssh
-#if [ "$0" = "/usr/sbin/lightdm-session" -a "$DESKTOP_SESSION" = "i3" ]; then
-#    export $(gnome-keyring-daemon --start --components=ssh)
-#fi
-
-eval $(/usr/bin/gnome-keyring-daemon  --components=gpg,pkcs11,secrets,ssh -r)                                                                                             1 ↵
-export GNOME_KEYRING_CONTROL GNOME_KEYRING_PID GPG_AGENT_INFO SSH_AUTH_SOCK
